1020c7b780993f44052c6f146a6430336e78a1e3
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-12-13 17:49:15 +0700

    upda App

--- CoreSpatial-master/ReadSpeedShpFile/App.cs
+++ CoreSpatial-master/ReadSpeedShpFile/App.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using static ReadSpeedShpFile.Common.Strings;
 using static ReadSpeedShpFile.Controller.HandleShpFile;
